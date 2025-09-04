11bc2a110252dd03a46ca180a7a2a0803721f1ef
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-04 22:52:25 +0500

    Add comments

---
+++ comments.py
@@ -0,0 +1,13 @@
+# Its a Single line comment in python .
+
+
+print("Hello G")
+print("Hello GitHub Learners")
+
+
+""" This is a multiline comment.
+
+every things inside a comments condition # or """ """
+"""
+
+
