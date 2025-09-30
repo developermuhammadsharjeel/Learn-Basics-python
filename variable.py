e91e550adf46ce8ae625c553cf91d3f21d78e9fb
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-09-30 19:48:20 +0500

    new

--- variable.py
+++ variable.py
@@ -3,6 +3,7 @@
 Python has no command for declaring a variable.
 
 A variable is created the moment you first assign a value to it.
+In python we dont need to add variable type with variable (Like in  c++ we add type of variable with them int a=10; string name = "name"; )
 '''
 
 x = 5
