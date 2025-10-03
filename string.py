9671b36feee28eb1808c8bc19d67a87643749b79
Muhammad Sharjeel <03-135232-059@student.bahria.edu.pk>
2025-10-03 12:10:46 +0500

    Add

---
+++ string.py
@@ -0,0 +1,16 @@
+## Simple Text (string)
+
+name = "Sharjeel"
+print("name")
+
+
+"""
+Multiline Strings, Used to print a paragraph in a sinle output
+"""
+
+paragraph = """ This is a paragraph. In this way we can make and run a multiline strings in strings.
+we acn add any type of the strings and paragraph in this area, simply we have to write the paragraph in three " so
+we could print it
+
+"""
+print(paragraph)
